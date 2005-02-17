d067ba5d4f9a8688ed078c43e1f56b9cac782f6c
John Mutchek <jmutchek@users.sourceforge.net>
2005-02-17 23:26:54 +0000

    added main() routine for command line processing

--- src/java/com/petebevin/markdown/MarkdownProcessor.java
+++ src/java/com/petebevin/markdown/MarkdownProcessor.java
@@ -673,7 +673,24 @@ public class MarkdownProcessor {
     }
 
     public String toString() {
-        return "Markdown Processor version 0.1.0";
+        return "Markdown Processor for Java (version 0.1.0)";
+    }
+    
+    public static void main(String[] args) {
+    	StringBuffer buf = new StringBuffer();
+    	char[] cbuf = new char[1024];   	
+    	java.io.Reader in = new java.io.InputStreamReader(System.in);
+    	try {
+	    	int charsRead = in.read(cbuf);
+	    	while (charsRead >= 0) {
+	    		buf.append(cbuf, 0, charsRead);
+	    		charsRead = in.read(cbuf);
+	    	}
+	    	System.out.println(new MarkdownProcessor().markdown(buf.toString()));
+    	} catch (java.io.IOException e) {
+    		System.err.println("Error reading input: " + e.getMessage());
+    		System.exit(1);
+    	}
     }
 }
 
