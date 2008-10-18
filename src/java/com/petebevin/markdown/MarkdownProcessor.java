myabc/markdownj

commit 6aa059590d18550ea90c45134bd5795fab9d06e0

Upgrade source to require Java 1.5
* Update java compiler options in build.xml to require Java 1.5.
* Add -Xlint:unchecked option.
* Add @Overrides annotations.

src/java/com/petebevin/markdown/MarkdownProcessor.java
@@ -437,7 +437,7 @@ public class MarkdownProcessor {
                     result = result.replaceAll("\\s+$", "");
                     
                     String html;
-                    if (listType == "ul") {
+                    if ("ui".equals(listType)) {
                         html = "<ul>" + result + "</ul>\n";
                     } else {
                         html = "<ol>" + result + "</ol>\n";
@@ -794,6 +794,7 @@ public class MarkdownProcessor {
         return ed.toString();
     }
 
+    @Override
     public String toString() {
         return "Markdown Processor for Java 0.3.0 (compatible with Markdown 1.0.2b2)";
     }

build.xml
@@ -80,7 +80,12 @@ software, even if advised of the possibility of such damage.
     <!-- compile source -->
     <target name="build" depends="init">
         <mkdir dir="${build}" />
-        <javac destdir="${build}" srcdir="${java}" />
+        <javac srcdir="${java}"
+               destdir="${build}"  
+               source="1.5"
+               target="1.5">
+            <compilerarg value="-Xlint"/>
+        </javac>
         <!-- copy non-java files, like .properties files, to build dir. -->
         <copy todir="${build}">
             <fileset dir="${java}" excludes="**/*.java" />
@@ -89,7 +94,9 @@ software, even if advised of the possibility of such damage.
 
     <!-- compile test source -->
     <target name="build-test" depends="build">
-        <javac srcdir="test/java">
+        <javac srcdir="test/java"
+               source="1.5"
+               target="1.5">
             <classpath refid="tools.class.path"/>
             <classpath>
                 <pathelement location="${build}" />

src/java/com/petebevin/markdown/CharacterProtector.java
@@ -77,6 +77,7 @@ class CharacterProtector {
         return sb.toString();
     }
 
+    @Override
     public String toString() {
         return protectMap.toString();
     }

src/java/com/petebevin/markdown/HTMLToken.java
@@ -28,6 +28,7 @@ public class HTMLToken {
         return text;
     }
 
+    @Override
     public String toString() {
         String type;
         if (isTag()) {

src/java/com/petebevin/markdown/LinkDefinition.java
@@ -52,6 +52,7 @@ public class LinkDefinition {
         return title;
     }
 
+    @Override
     public String toString() {
         return url + " (" + title + ")";
     }

src/java/com/petebevin/markdown/MarkdownFilter.java
@@ -35,7 +35,6 @@ software, even if advised of the possibility of such damage.
 */
 package com.petebevin.markdown;
 
-import java.io.IOException;
 import java.io.Reader;
 
 import org.apache.tools.ant.filters.BaseParamFilterReader;
@@ -115,6 +114,7 @@ public class MarkdownFilter extends BaseParamFilterReader implements ChainableRe
      * @exception IOException if the underlying stream throws an IOException
      * during reading
      */
+    @Override
 	public final int read() throws java.io.IOException {
 		
 		if (chars == null) {

src/java/com/petebevin/markdown/TextEditor.java
@@ -62,6 +62,7 @@ public class TextEditor {
      * Give up the contents of the TextEditor.
      * @return
      */
+    @Override
     public String toString() {
         return text.toString();
     }

test/java/com/petebevin/markdown/test/EdgeCases.java
@@ -43,6 +43,7 @@ import java.util.regex.Pattern;
 public class EdgeCases extends TestCase {
     private MarkdownProcessor m;
 
+    @Override
     public void setUp() {
         m = new MarkdownProcessor();
     }

commit e1e7eedb86ed75cf1957ccb8c4d11df5a42dadae

Fixed typo introduced in 6aa0595
* resolves two test failures, where list tags were not being
  handled correctly.

src/java/com/petebevin/markdown/MarkdownProcessor.java
@@ -437,7 +437,7 @@ public class MarkdownProcessor {
                     result = result.replaceAll("\\s+$", "");
 
                     String html;
-                    if ("ui".equals(listType)) {
+                    if ("ul".equals(listType)) {
                         html = "<ul>" + result + "</ul>\n";
                     } else {
                         html = "<ol>" + result + "</ol>\n";